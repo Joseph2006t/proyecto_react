d971ffcb3034c99aa78b399038babe3b8111cb1d
Joseph2006t <laizatellojoseph@gmail.com>
2024-12-15 15:53:43 -0500

    proyecto react curso

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import Index from "./pages/Index"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Registro from "./pages/Registro";
+function App() {
+
+  return (
+    <Router>
+      <main>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Index/>}/>
+          <Route path="/iniciar-sesion" element={<Login/>}/>
+          <Route path="/registro" element={<Registro/>}/>
+        </Routes>
+        <Footer/>
+      </main>
+
+    </Router>
+  )
+}
+
+export default App
